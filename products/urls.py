904b76cd46612563b4ab6aeaed0b3f3993062a00
Pelayo156 <basti.molina1@gmail.com>
2024-06-20 15:26:28 -0400

    Agregando vistas y urls para navegar por ellas

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index, name="index")
+    path('index/', views.index, name="index"),
+    path('about/', views.about, name="about"),
+    path('dog/', views.dog, name="dog"),
+    path('cat/', views.cat, name="cat"),
+    path('cart/', views.cart, name="cart")
 ]
--- products/views.py
+++ products/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # View para página principal
 def index(request):
     return render(request, 'products/index.html')
+
+# View para página nosotros
+def about(request):
+    return render(request, 'products/about.html')
+
+# View para página perro
+def dog(request):
+    return render(request, 'products/dog.html')
+
+# View para página gato
+def cat(request):
+    return render(request, 'products/cat.html')
+
+# View para página de carro
+def cart(request):
+    return render(request, 'products/cart.html')
